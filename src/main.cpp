c8f624c7aaeeafa7a309353f7e94827728a5ef15
PraveenKumar-Rajendran <aravindhanpraveen19@gmail.com>
2021-07-17 17:30:02 +0900

    add: improve speed

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include "json.hpp"
 #include "spline.h"
 
-#define MAX_SPEED 50.0
+#define MAX_SPEED 49.5
 #define SAFETY_DISTANCE 20.0
 
 
@@ -175,7 +175,7 @@ int main() {
             // shift to right lane if we are not in the right most lane and right lane is safe to shift            
             else if(right_shift_safe)  {lane_num+=1;}
             // if no lane is available, follow the front car and decelerate(with the given limit) to avoid collision.
-            else{ if( ahead_car_speed < (MAX_SPEED + 1) ){ref_velocity_mph -= 0.224;} }
+            else{ if( ahead_car_speed < MAX_SPEED ){ref_velocity_mph -= 0.224;} }
 
           }
 
