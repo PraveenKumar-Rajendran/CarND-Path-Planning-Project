c235c3dee11214a36821e7ac91360e3f5f48e8e8
PraveenKumar-Rajendran <aravindhanpraveen19@gmail.com>
2021-07-17 14:32:02 +0900

    add: h.onMessage

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main() {
   int lane_num = 1; // default lane
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+               &map_waypoints_dx,&map_waypoints_dy,&ref_velocity_mph,&lane_num]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
